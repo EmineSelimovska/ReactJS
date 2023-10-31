63134895ce44e30837249b70b5c490c9983a7c6b
EmineSelimovska <114228146+EmineSelimovska@users.noreply.github.com>
2023-10-31 12:49:14 +0200

    Update Timer.jsx

--- Components Basic Idea/src/components/Timer.jsx
+++ Components Basic Idea/src/components/Timer.jsx
@@ -5,6 +5,7 @@ export default function Timer(props){
   
    
     // let time = 0;
+    // Note: Don't use setTimeout , useEffect is more appropriate!
      setTimeout (() => {
         setTime(time + 1);
 
