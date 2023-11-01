e3276fb22aa2fd2e5c938d227d79df344bec378e
EmineSelimovska <114228146+EmineSelimovska@users.noreply.github.com>
2023-11-01 22:01:02 +0200

    add useEffect

--- Components Deep Dive/src/App.jsx
+++ Components Deep Dive/src/App.jsx
@@ -1,23 +1,47 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css'
 
 
 function App() {
-  
-  let [numbers, setNumbers] = useState([1,2,3,4,5]);
 
- 
-const onClick = () => {
-  setNumbers(oldState => oldState.slice(1));
-}
+  let [numbers, setNumbers] = useState([1, 2, 3, 4, 5]);
+  let [count, setCount] = useState(0);
 
-return (
+  useEffect(() => {
+    console.log('Mount components');
+  }, [])
 
-  <div>
-    <ul>
-      {numbers.map((number, index) => <li data-key={index} key={index}>{number * 2}</li>)}
-    </ul>
-  <button  onClick={onClick}>Remove</button>
+  useEffect(() => {
+    console.log('Update components - numbers');
+  }, [numbers, count])
+
+
+  useEffect(() => {
+
+
+    setTimeout(() => setCount(s => s + 1), 1000);
+
+
+  }, [count])
+
+  const onClick = () => {
+    setNumbers(oldState => oldState.slice(0, oldState.length - 1));
+
+  }
+
+  if (!numbers.length) {
+    return null;
+  }
+  return (
+
+    <div>
+      <h3>Count: {count}</h3>
+      <ul>
+        {numbers.map((number, index) => <li data-key={index} key={index}>{number * 2}</li>)}
+      </ul>
+      <button onClick={onClick}>Remove</button>
+      <button onClick={() => setCount(c => c + 1)}>+</button>
+
     </div>
   )
 }
